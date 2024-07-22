60d0dfea40bb403270e476bd4f90f8d5a75eb2f5
Steven Lim <limstevenlbw@gmail.com>
2024-07-22 16:09:02 -0700

    fixing ui, player can move, added a song, fixed video material

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,6 +8,9 @@ public class Player : MonoBehaviour
     public float yaw;
     public float pitch;
     public Camera playerCamera;
+    public InputFieldPrompt inputField;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -22,10 +25,32 @@ public class Player : MonoBehaviour
         playerCamera.transform.localEulerAngles = new Vector3(pitch, 0, 0);
    
     }
+
     // Update is called once per frame
     void Update()
     {
         CameraControl();
+        Vector3 direction = Vector3.zero;
 
+        if (inputField.activated) return;
+
+        if (Input.GetKey("w"))
+        {
+            direction += transform.forward;
+        }
+        if (Input.GetKey("s"))
+        {
+            direction -= transform.forward;
+        }
+        if (Input.GetKey("d"))
+        {
+            direction += transform.right;
+        }
+        if (Input.GetKey("a"))
+        {
+            direction -= transform.right;
+        }
+
+        transform.position += direction * 4.5f * Time.deltaTime;
     }
 }
--- Assets/Scripts/VideoLoader.cs
+++ Assets/Scripts/VideoLoader.cs
@@ -9,12 +9,21 @@ public class VideoLoader : MonoBehaviour
     public VideoPlayer videoPlayer;
     public RawImage videoImage;
     public string videoUrl = "https://videos.pexels.com/video-files/6924608/6924608-hd_1080_1920_24fps.mp4";
+
+    private bool isMuted;
+
     // Start is called before the first frame update
     void Start()
+    {
+        PlayVideo();
+    }
+
+    void PlayVideo()
     {
         videoPlayer.url = videoUrl;
         videoPlayer.audioOutputMode = VideoAudioOutputMode.AudioSource;
         videoPlayer.EnableAudioTrack(0, true);
+        videoPlayer.SetDirectAudioVolume(0, 0.3f);
         videoPlayer.Prepare();
     }
 
@@ -24,6 +33,12 @@ public class VideoLoader : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.H))
         {
             videoImage.enabled = !videoImage.enabled;
+        }
+
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            if(isMuted) videoPlayer.SetDirectAudioVolume(0, 0.3f);
+            else videoPlayer.SetDirectAudioVolume(0, 0);  //Mute the audio
         }
     }
 }
